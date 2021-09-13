116b048f8c66d28f58ae5770b3e55f6f05e3d577
zaki <ahmedzakariya355@gmail.com>
2021-09-13 13:56:04 -0400

    Implenting collision with the three types of terrarian
    Right now it's kinda dumb. Will look for a better way to implmetn this
    in future project

---
+++ Assets/collision.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class collision : MonoBehaviour
+{
+    public BoxCollider2D collider;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log(this.collider.tag);
+        
+
+    }
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        Debug.Log(other.collider.tag);
+        
+    }
+}
